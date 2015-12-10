400c6ae00e5c4f52849d7fc754a05ca85c57e1d7
Patrick Wong <Patrick Wong>
2015-12-09 16:01:07 -0800

    MAPR-21517 - need additional control over generated POM for kafka-clients

---
+++ buildSrc/src/main/groovy/PomUtil.groovy
@@ -0,0 +1,54 @@
+public class PomUtil {
+    public static final List<String> forbiddenElements = [ "project",
+        "modelVersion",
+        "artifactId",
+        "groupId",
+        "version"
+    ]
+
+    public static boolean listContains(String checkedString) {
+        for (String masterString : forbiddenElements) {
+            if (checkedString.contains(masterString)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    public static List<Node> parsePom(File originalPom) {
+        List<Node> cleanedNodes = new Vector<String>()
+        Node originalPomNode = new XmlParser().parseText(originalPom.text)
+        originalPomNode.children().each {
+            Node originalElement ->
+            String elementName = originalElement.name().toString()
+            if (!(listContains(elementName))) {
+                cleanedNodes.add(originalElement)
+            }
+        }
+        return cleanedNodes
+    }
+
+    public static List<Node> parsePom(File originalPom, String maprVersion) {
+        List<Node> cleanedNodes = parsePom(originalPom)
+        for (Node node : cleanedNodes) {
+            if (node.name().toString().contains("dependencies")) {
+                node.append(maprStreams(maprVersion))
+            }
+        }
+        return cleanedNodes
+    }
+
+    public static List<Node> parsePom(String originalPom) {
+        File originalPomFile = new File(originalPom)
+        return parsePom(originalPomFile)
+    }
+    
+    public static Node maprStreams(String maprVersion) {
+        Node mrsNode = new NodeBuilder().dependency {
+            groupId "com.mapr.streams"
+            artifactId "mapr-streams"
+            version maprVersion
+        }
+        return mrsNode
+    }
+}
