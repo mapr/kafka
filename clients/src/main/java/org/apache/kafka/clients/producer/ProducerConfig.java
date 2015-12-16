d1c5bec19502889dd7668097ef6331d56c5e0039
Pindikura Ravindra <nagrawal@qa-node138.qa.lab>
2015-12-16 12:20:47 +0000

    MAPR-20370: add producer and timestamp to messages MAPR-21464: provide a sofmount option for streams clients

--- clients/src/main/java/org/apache/kafka/clients/producer/ProducerConfig.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/ProducerConfig.java
@@ -201,6 +201,10 @@ public class ProducerConfig extends AbstractConfig {
     private static final String REQUEST_TIMEOUT_MS_DOC = CommonClientConfigs.REQUEST_TIMEOUT_MS_DOC;
 
     /** STREAMS SPECIFIC SETTINGS **/
+    /** <code>streams.rpc.timeout.ms</code> */
+    public static final String STREAMS_RPC_TIMEOUT_MS_CONFIG = CommonClientConfigs.STREAMS_RPC_TIMEOUT_MS_CONFIG;
+    private static final String STREAMS_RPC_TIMEOUT_MS_DOC = CommonClientConfigs.STREAMS_RPC_TIMEOUT_MS_DOC;
+
     /** <code>streams.buffer.max.time.ms</code> **/
     public static final String STREAMS_BUFFER_TIME_CONFIG = "streams.buffer.max.time.ms";
     private static final String STREAMS_BUFFER_TIME_DOC = "Messages are buffered in the producer at most the specified time.  A thread "
@@ -308,6 +312,12 @@ public class ProducerConfig extends AbstractConfig {
                                         CommonClientConfigs.SECURITY_PROTOCOL_DOC)
                                 .withClientSslSupport()
                                 .withClientSaslSupport()
+                                .define(STREAMS_RPC_TIMEOUT_MS_CONFIG,
+                                        Type.INT,
+                                        Integer.MAX_VALUE,
+                                        atLeast(30000),
+                                        Importance.LOW,
+                                        STREAMS_RPC_TIMEOUT_MS_DOC)
                                 .define(STREAMS_BUFFER_TIME_CONFIG,
                                         Type.LONG,
                                         3000L,
