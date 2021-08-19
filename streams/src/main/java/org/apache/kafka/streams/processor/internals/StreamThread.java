290bf71f61d924a8a5d005bb495a50a81aa19959
Tetiana Fioshkina <fioshkina.tany@gmail.com>
2021-08-19 17:59:41 +0300

    KAFKA-746 Remove workaround implemented due to the difference in work of poll(0) between Mapr and Apache Kafka.

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
@@ -637,10 +637,6 @@ public class StreamThread extends Thread {
 
         final long pollLatency = advanceNowAndComputeLatency();
 
-        if (taskManager.needsInitializationOrRestoration()) {
-            taskManager.tryToCompleteRestoration();
-        }
-
         if (records != null && !records.isEmpty()) {
             pollSensor.record(pollLatency, now);
             pollRecordsSensor.record(records.count(), now);
